2602438557f8d47acfeffa315d2537e98e7f9437
Eric Vazquez <ericvazquez7992@gmail.com>
2024-12-02 18:45:57 -0500

    forgot to add it in authservice.js..

--- vue/src/services/AuthService.js
+++ vue/src/services/AuthService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: 'https://3.137.41.111:9000' // Set to your EC2 instance's public IP
+  baseURL: 'https://3.134.64.215:9000' // Set to your EC2 instance's public IP
 });
 
 export default {
