ee7b783b24edab5abafea6844537b78718123aeb
Eric Vazquez <ericvazquez7992@gmail.com>
2024-12-02 02:35:07 -0500

    added baseurl

--- vue/src/services/AuthService.js
+++ vue/src/services/AuthService.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
 
+const http = axios.create({
+  baseURL: 'http://3.137.41.111:9000' // Set to your EC2 instance's public IP
+});
+
 export default {
 
   login(user) {
-    return axios.post('/login', user)
+    return http.post('/login', user)
   },
 
   register(user) {
-    return axios.post('/register', user)
+    return http.post('/register', user)
   },
 
   getUserDetails(token){
-    return axios.get('/user-details',{
+    return http.get('/user-details',{
       headers:{
         Authorization:`Bearer ${token}`,
       }
